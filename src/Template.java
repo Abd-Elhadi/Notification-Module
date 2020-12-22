a9cf9d1efe809c1e895ac3b87a1aabcba7a1aab9
samar <samareeyassin155@gmail.com>
2020-12-22 22:44:12 +0200

    update template function added

--- src/Template.java
+++ src/Template.java
@@ -2,34 +2,56 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Template {
+    static int ID=0;
     File templates = new File("templates.txt");
     FileWriter myWriter = new FileWriter("templates.txt");
 
 
     public Template() throws IOException {
-        myWriter.write("Language - Type - Content\n");
-        myWriter.close();
+//        myWriter.write("ID - Language - Type - Content\n");
+//        myWriter.close();
     }
 
+
     public String getContent(String templateName) throws IOException {
-        String type,content,language,line;
+        String name,content,language,line,ID;
         FileReader file = new FileReader("templates.txt");
         BufferedReader br = new BufferedReader(file);
         while ((line = br.readLine())!= null){
             String[] data = line.split(" - ");
-            language = data[0];
-            type = data[1];
-            content = data[2];
-            if (templateName.equalsIgnoreCase(type))return content;
+            ID= data[0];
+            language = data[1];
+            name = data[2];
+            content = data[3];
+            if (templateName.equalsIgnoreCase(name))return content;
 
         }
 
         return "NOT FOUND";
     }
 
+    public  String getContent(int id) throws IOException {
+        String type,content,language,line;
+        int ID;
+        FileReader file = new FileReader("templates.txt");
+        BufferedReader br = new BufferedReader(file);
+        while ((line = br.readLine())!= null){
+            String[] data = line.split(" - ");
+            ID= Integer.parseInt(data[0]);
+            language = data[1];
+            type = data[2];
+            content = data[3];
+            if (id==ID)return content;
+
+        }
+
+        return "NOT FOUND";
+
+    }
+
     public void addTemplate(String Language , String templateName , String content) throws IOException {
         myWriter = new FileWriter("templates.txt",true);
-        myWriter.write(Language+" - "+templateName+" - " + content);
+        myWriter.write(ID++ + " - "+Language+" - "+templateName+" - " + content);
         myWriter.write("\n");
         myWriter.close();
 
@@ -37,19 +59,28 @@ public class Template {
 
     }
 
-    public void removeTemplate(String templateName) throws IOException {
+    public void updateTemplate(int ID , String language ,String newName ,String newContent) throws IOException {
+        removeTemplate(ID);
+        addTemplate(language,newName,newContent);
+    }
+
+
+
+    public void removeTemplate(int ID) throws IOException {
         File tempFile= new File("tempFile.txt");
         FileWriter tempWriter = new FileWriter("tempFile.txt");
-        String type,content,language,line;
+        String name,content,language,line;
+        int id;
         FileReader file = new FileReader("templates.txt");
         BufferedReader br = new BufferedReader(file);
         while ((line = br.readLine())!= null){
             String[] data = line.split(" - ");
-            language = data[0];
-            type = data[1];
-            content = data[2];
-            if (!(templateName.equalsIgnoreCase(type))) {
-                tempWriter.write(language+" - "+templateName+" - " + content);
+            id= Integer.parseInt(data[0]);
+            language = data[1];
+            name = data[2];
+            content = data[3];
+            if (id != ID) {
+                tempWriter.write(language+" - "+name+" - " + content);
                 tempWriter.write("\n");
             }
 
@@ -58,14 +89,14 @@ public class Template {
         tempWriter.close();
 
         if (!templates.delete())
-                System.out.println("Could not delete file");
+            System.out.println("Could not delete file");
 
 
         if (!tempFile.renameTo(templates))
             System.out.println("Could not rename file");
 
-
-
     }
+
+
 
 }
